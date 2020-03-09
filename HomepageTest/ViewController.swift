e528fcd6a7e0ab67d0afc2a78ed0eaccef47ed10
esther <rwarroar357@gmail.com>
2020-03-09 00:10:19 -0700

    need to make the buttons open another page

--- HomepageTest/ViewController.swift
+++ HomepageTest/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var csuTitlePic: UIImageView!
+    
+    @IBOutlet weak var homeStackView: UIStackView!
+    
+    @IBOutlet weak var stackButtonTools: UIButton!
+    @IBOutlet weak var stackButtonAbout: UIButton!
+    
+    @IBOutlet weak var stackButtonResources: UIButton!
+    
+    @IBOutlet weak var stackButtonContacts: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
 }
 
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ HomepageTest/homepageMenu.swift
@@ -0,0 +1,13 @@
+//
+//  homepageMenu.swift
+//  HomepageTest
+//
+//  Created by E.c on 3/6/20.
+//  Copyright © 2020 E.c. All rights reserved.
+//
+
+import UIKit
+
+class homepageMenu: UICollectionViewCell {
+    
+}
